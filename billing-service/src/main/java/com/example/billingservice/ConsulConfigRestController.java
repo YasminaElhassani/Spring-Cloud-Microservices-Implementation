35c2b08a1924c6bfc56534b07766c08eb2ceed16
yasmina <[ELHASSANI_YASMINA@emsi-edu.ma]>
2024-01-11 21:39:01 +0100

    fifth commit

--- billing-service/src/main/java/com/example/billingservice/ConsulConfigRestController.java
+++ billing-service/src/main/java/com/example/billingservice/ConsulConfigRestController.java
@@ -5,15 +5,20 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.Objects;
+
 @RestController
 @RefreshScope
 public class ConsulConfigRestController {
     @Autowired
     private MyConsulConfig myConsulConfig;
+    @Autowired
+    private MyVaultConfig myVaultConfig;
 
     @GetMapping("myConfig")
-    public MyConsulConfig myConsulConfig() {
-        return myConsulConfig;
+    public Map<String, Object> myConfig(){
+        return Map.of("Vault Config",myVaultConfig,"Consul Config",myConsulConfig);
     }
 }
 
---
+++ billing-service/src/main/java/com/example/billingservice/MyVaultConfig.java
@@ -0,0 +1,15 @@
+package com.example.billingservice;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "user")
+public class MyVaultConfig {
+    private String username;
+    private String password;
+    private String otp;
+
+}
